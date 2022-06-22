1b022e26d1317a8e8895e72010d54f980efcce64
Stanislav Surskiy <ssurskiy@gmail.com>
2022-06-23 00:56:52 +0300

    feat: adds support specialities collection property of the CompanyDTO
    Adds:
    - Specialities DTO
    - Specialities collection caster

---
+++ src/DTO/Company/SpecialtiesDTO.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Muscobytes\CoresignalDbApi\DTO\Company;
+
+use Carbon\Carbon;
+use Muscobytes\CoresignalDbApi\Casts\CarbonCaster;
+use Spatie\DataTransferObject\Attributes\CastWith;
+use Spatie\DataTransferObject\Attributes\Strict;
+use Spatie\DataTransferObject\DataTransferObject;
+
+#[Strict]
+class SpecialtiesDTO extends DataTransferObject
+{
+    public int $id;
+
+    public int $company_id;
+
+    public string $specialty;
+
+    #[CastWith(CarbonCaster::class)]
+    public Carbon $created;
+
+    #[CastWith(CarbonCaster::class)]
+    public Carbon $last_updated;
+
+    public int $deleted;
+}
